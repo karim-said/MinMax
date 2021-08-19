df8a8c10a253b89a43e928d5c07c47c1a281ac5d
Karim Said <inkriment@gmail.com>
2021-08-19 19:47:53 +0500

    MinMax10

--- Arr/Program.cs
+++ Arr/Program.cs
@@ -1,42 +1,41 @@
 ﻿using System;
 using System.Runtime.InteropServices;
- //MinMax9==================================================
+ //MinMax10==================================================
 namespace Arr
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int f_max, l_max, now;
-            int N, f_i = 1, l_i = 1;
+            int min, max, now;
+            int N, imax = 1, imin = 1;
 
             Console.Write("You need enter a value for N variable ");
 
             N = Convert.ToInt32(Console.ReadLine());
             Console.Write("now = ");
             now = Convert.ToInt32(Console.ReadLine());
-            f_max = now;
-            l_max = now;
+            min = now;
+            max = now;
 
             for (int i = 1; i < N; i++)
             {
                 Console.Write("now = ");
                 now = Convert.ToInt32(Console.ReadLine());
 
-                if (now > f_max)
+                if (now < min)
                 {
-                    f_i = i + 1;
-                    f_max = now;
+                    imax = i + 1;
+                    min = now;
                 }
-
-                if (now >= l_max)
+                if (now > max)
                 {
-                    l_i = i + 1;
-                    l_max = now;
+                    imin = i + 1;
+                    max = now;
                 }
             }
 
-            Console.WriteLine($"f_i = {f_i}\nl_i = {l_i}\n");
+            Console.WriteLine("ex = {0}\n", imin < imax ? imin : imax);
             Console.ReadKey();
         }
     }
