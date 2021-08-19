4edba00e245fca8ea2b91481ead62e018e09487e
Karim Said <inkriment@gmail.com>
2021-08-19 19:44:36 +0500

    MinMax8

--- Arr/Program.cs
+++ Arr/Program.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 using System.Runtime.InteropServices;
- //MinMax7==================================================
+ //MinMax8==================================================
 namespace Arr
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double p_max, l_min, now;
-            int N, i_max = 1, i_min = 1;
+            int f_min, l_min, now;
+            int N, f_i = 1, l_i = 1;
 
             Console.Write("You need enter a value for N variable ");
 
             N = Convert.ToInt32(Console.ReadLine());
             Console.Write("now = ");
-            now = double.Parse(Console.ReadLine());
-            p_max = now;
+            now = Convert.ToInt32(Console.ReadLine());
+            f_min = now;
             l_min = now;
 
             for (int i = 1; i < N; i++)
             {
                 Console.Write("now = ");
-                now = double.Parse(Console.ReadLine());
+                now = Convert.ToInt32(Console.ReadLine());
 
-                if (now > p_max)
+                if (now < f_min)
                 {
-                    i_max = i + 1;
-                    p_max = now;
+                    f_i = i + 1;
+                    f_min = now;
                 }
 
                 if (now <= l_min)
                 {
-                    i_min = i + 1;
+                    l_i = i + 1;
                     l_min = now;
                 }
             }
 
-            Console.WriteLine($"i_max = {i_max}\ni_min = {i_min}\n");
+            Console.WriteLine($"f_i = {f_i}\nl_i = {l_i}\n");
             Console.ReadKey();
         }
     }
