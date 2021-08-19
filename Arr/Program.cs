429b01b8f4ca6835c67199ea60bc43021c23b50d
Karim Said <inkriment@gmail.com>
2021-08-19 19:43:19 +0500

    MinMax6

--- Arr/Program.cs
+++ Arr/Program.cs
@@ -1,39 +1,42 @@
 ﻿using System;
 using System.Runtime.InteropServices;
- //MinMax5==================================================
+ //MinMax6==================================================
 namespace Arr
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double m, v, P_max = 0;
-            int N, _i;
+            double p_min, l_max, now;
+            int N, i_p = 1, i_l = 1;
 
             Console.Write("You need enter a value for N variable ");
 
             N = Convert.ToInt32(Console.ReadLine());
-            Console.Write("m = ");
-            m = double.Parse(Console.ReadLine());
-            Console.Write("v = ");
-            v = double.Parse(Console.ReadLine());
-            P_max = m / v;
-            _i = 1;
+            Console.Write("now = ");
+            now = double.Parse(Console.ReadLine());
+            p_min = now;
+            l_max = now;
 
             for (int i = 1; i < N; i++)
             {
-                Console.Write("m = ");
-                m = double.Parse(Console.ReadLine());
-                Console.Write("v = ");
-                v = double.Parse(Console.ReadLine());
-                if (m / v > P_max)
-                {
-                    P_max = m / v;
-                    _i = i + 1;
+                Console.Write("now = ");
+                now = double.Parse(Console.ReadLine());
 
+                if (now < p_min)
+                {
+                    i_p = i + 1;
+                    p_min = now;
+                }
+
+                if (now >= l_max)
+                {
+                    i_l = i + 1;
+                    l_max = now;
                 }
             }
-            Console.WriteLine($"Num {_i}     P_max = {P_max}\n");
+
+            Console.WriteLine($"i_p = {i_p}\ni_l = {i_l}\n");
             Console.ReadKey();
 
         }
