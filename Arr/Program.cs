91344b4922f8cb7f6d55585c4f1fde7e7a581f43
Karim Said <inkriment@gmail.com>
2021-08-19 19:42:31 +0500

    MinMax5

--- Arr/Program.cs
+++ Arr/Program.cs
@@ -1,33 +1,39 @@
 ﻿using System;
 using System.Runtime.InteropServices;
- //MinMax4==================================================
+ //MinMax5==================================================
 namespace Arr
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double a, now;
-            int N, _i = 1;
+            double m, v, P_max = 0;
+            int N, _i;
 
             Console.Write("You need enter a value for N variable ");
+
             N = Convert.ToInt32(Console.ReadLine());
-            Console.Write("a = ");
-            now = double.Parse(Console.ReadLine());
+            Console.Write("m = ");
+            m = double.Parse(Console.ReadLine());
+            Console.Write("v = ");
+            v = double.Parse(Console.ReadLine());
+            P_max = m / v;
+            _i = 1;
 
-            for (int i = 1; i <= N - 1; i++)
+            for (int i = 1; i < N; i++)
             {
-                Console.Write("a = ");
-                a = double.Parse(Console.ReadLine());
-
-                if (a < now)
+                Console.Write("m = ");
+                m = double.Parse(Console.ReadLine());
+                Console.Write("v = ");
+                v = double.Parse(Console.ReadLine());
+                if (m / v > P_max)
                 {
+                    P_max = m / v;
                     _i = i + 1;
-                    now = a;
+
                 }
             }
-
-            Console.WriteLine($"min index num = {_i}\n");
+            Console.WriteLine($"Num {_i}     P_max = {P_max}\n");
             Console.ReadKey();
 
         }
