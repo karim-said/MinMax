439855aad9d99620875440809f80821e52de51ef
Karim Said <inkriment@gmail.com>
2021-08-27 15:31:57 +0500

    MinMax15

--- Arr/Program.cs
+++ Arr/Program.cs
@@ -1,39 +1,42 @@
 ﻿using System;
 using System.Runtime.InteropServices;
- //MinMax14 ==================================================
+ //MinMax15 ==================================================
 namespace Arr
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double min=0,i_=0, now,B;
+            double max=0,i_=0, now,B,C;
             bool flag=true;
 
-          
+         
 
             Console.Write("You need enter a value for B variable ");
             B = Convert.ToDouble(Console.ReadLine());
+            Console.Write("You need enter a value for C variable ");
+            C = Convert.ToDouble(Console.ReadLine());
 
             for (int i = 1; i <= 10; i++)
             {
                           
                 Console.Write("now = ");
                 now = Convert.ToDouble(Console.ReadLine());
-                if(now>B && flag)
+                if(now>B && now < C && flag)
                 {
-                    min = now;
+                    max = now;
+                    i_ = i;
                     flag = false;
                 }    
-                if(now > B && now <= min)
+                if(now > B && now < C && now > max)
                 {
-                    min = now;
+                    max = now;
                     i_ = i;
                 }
                                 
             }
 
-            Console.WriteLine("min -> " + min + "            " + "i_ -> " + i_);
+            Console.WriteLine("max -> " + max + "            " + "i_ -> " + i_);
             Console.ReadKey();
         }
     }
